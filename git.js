be1774462ea9696751c1af7a5215ad90b8d8ef84
harryzeng <harryzeng90@gmail.com>
2024-03-30 13:18:26 +0800

    test: test2

--- git.js
+++ git.js
@@ -3,7 +3,7 @@ const execSync = require('child_process').execSync
 const path = require('path')
 const pullAction = function () {
         const timer = setTimeout(function () {
-                // ceshiyixia
+                // ceshiyixiakdk
 		// 要拉取的仓库在本地的路径
 		// --git-dir 要拉取的仓库中.git在本地的路径
                 execSync('git --work-tree="../js/js-summary" --git-dir="../js/js-summary/.git" pull')
