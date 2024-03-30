dcc47f1d17bc2d718ab68cf02f13aaab5e7134f8
harryzeng <harryzeng90@gmail.com>
2024-03-30 14:46:42 +0800

    test: bb分支上的提交1

--- git.js
+++ git.js
@@ -3,6 +3,7 @@ const execSync = require('child_process').execSync
 const path = require('path')
 const pullAction = function () {
         const timer = setTimeout(function () {
+                // bb分支上的提交
 		// 要拉取的仓库在本地的路径
 		// --git-dir 要拉取的仓库中.git在本地的路径
                 execSync('git --work-tree="../js/js-summary" --git-dir="../js/js-summary/.git" pull')
