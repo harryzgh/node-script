1c06a7648c93322e38b169a5ec49a3a9eb252cad
harryzeng <harryzeng90@gmail.com>
2024-03-30 11:19:51 +0800

    style: change log text

--- git.js
+++ git.js
@@ -1,12 +1,15 @@
-// 定时从远端仓库拉取代码
-var execSync = require('child_process').execSync
-var pullAction = function () {
-        var timer = setTimeout(function () {
+//  定时从远端仓库拉取代码
+const execSync = require('child_process').execSync
+const path = require('path')
+const pullAction = function () {
+        const timer = setTimeout(function () {
 		// 要拉取的仓库在本地的路径
 		// --git-dir 要拉取的仓库中.git在本地的路径
                 execSync('git --work-tree="../js/js-summary" --git-dir="../js/js-summary/.git" pull')
                 pullAction()
-                console.log('test++++')
+                console.log('test++++00', process.cwd())
+		console.log('test++++11', __dirname)
+                console.log('9290+++++', path.resolve(__dirname, "../public/index.html"))
         }, 3000)
 }
 pullAction()
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('test00', process.cwd())
+console.log('test11', __dirname)
+console.log('test11', __filename)
