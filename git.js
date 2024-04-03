f7f802d2275c6bbd7dfbbb3034a8fd5baee1e74b
harryzeng <harryzeng90@gmail.com>
2024-04-03 10:18:22 +0800

    style: 删除无用代码,添加注释

--- git.js
+++ git.js
@@ -3,15 +3,10 @@ const execSync = require('child_process').execSync
 const path = require('path')
 const pullAction = function () {
         const timer = setTimeout(function () {
-                // zgh分支的提交1
-                // ceshiyixiakdk
-		// 要拉取的仓库在本地的路径
+		// --work-tree 要拉取的仓库在本地的路径
 		// --git-dir 要拉取的仓库中.git在本地的路径
                 execSync('git --work-tree="../js/js-summary" --git-dir="../js/js-summary/.git" pull')
                 pullAction()
-                console.log('test++++00', process.cwd())
-		console.log('test++++11', __dirname)
-                console.log('9290+++++', path.resolve(__dirname, "../public/index.html"))
         }, 3000)
 }
 pullAction()
--- test.js
+++
@@ -1,3 +0,0 @@
-console.log('test00', process.cwd())
-console.log('test11', __dirname)
-console.log('test11', __filename)
